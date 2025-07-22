a3b97a8b18cd3e5d91cf261b64eb8b0257c531e9
MYH <ansdudgb@gmail.com>
2025-07-23 01:00:55 +0900

    feat(front): 게시물 작성 페이지 컴포넌트 구현, 게시물 작성 API 연동

--- board-front/src/apis/index.ts
+++ board-front/src/apis/index.ts
@@ -3,6 +3,8 @@ import { SignInRequestDto, SignUpRequestDto } from "./request/auth";
 import { SignInResponseDto, SignUpResponseDto } from "./response/auth";
 import { ResponseDto } from "./response";
 import { GetSignInUserResponseDto } from "./response/user";
+import { PostBoardRequestDto } from "./request/board";
+import { PostBoardResponseDto } from "./response/board";
 
 const DOMAIN = "http://localhost:4000";
 
@@ -16,6 +18,7 @@ const authorizationHeader = (accessToken: string) => {
   };
 };
 
+//          description: authorization          //
 const SIGN_IN_URL = () => `${API_DOMAIN}/auth/sign-in`;
 const SIGN_UP_URL = () => `${API_DOMAIN}/auth/sign-up`;
 
@@ -48,7 +51,7 @@ export const signUpRequest = async (requestBody: SignUpRequestDto) => {
     });
   return result;
 };
-
+//          description: user          //
 const GET_SIGN_IN_USER_URL = () => `${API_DOMAIN}/user`;
 
 export const getSignInUserRequest = async (accessToken: string) => {
@@ -64,5 +67,50 @@ export const getSignInUserRequest = async (accessToken: string) => {
       return responseBody;
     });
 
+  return result;
+};
+
+//          description: board          //
+const POST_BOARD_URL = () => `${API_DOMAIN}/board`;
+
+export const postBoardRequest = async (
+  requestBody: PostBoardRequestDto,
+  accessToken: string
+) => {
+  const result = await axios
+    .post(POST_BOARD_URL(), requestBody, authorizationHeader(accessToken))
+    .then((response) => {
+      const responseBody: PostBoardResponseDto = response.data;
+      return responseBody;
+    })
+    .catch((error) => {
+      if (!error.response) return null;
+      const responseBody: ResponseDto = error.response.data;
+      return responseBody;
+    });
+  return result;
+};
+
+const FILE_DOMAIN = `${DOMAIN}/file`;
+
+const FILE_UPLOAD_URL = () => `${FILE_DOMAIN}/upload`;
+
+const multipartHeaders = {
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
+};
+
+//         description: file          //
+export const fileUploadRequest = async (data: FormData) => {
+  const result = await axios
+    .post(FILE_UPLOAD_URL(), data, multipartHeaders)
+    .then((response) => {
+      const responseBody = response.data;
+      return responseBody;
+    })
+    .catch((error) => {
+      return null;
+    });
   return result;
 };
--- board-front/src/layouts/Header/index.tsx
+++ board-front/src/layouts/Header/index.tsx
@@ -19,6 +19,10 @@ import {
 } from "constant";
 import { useCookies } from "react-cookie";
 import { useBoardStore, useLoginUserStore } from "stores";
+import { fileUploadRequest, postBoardRequest } from "apis";
+import { PostBoardRequestDto } from "apis/request/board";
+import { ResponseDto } from "apis/response";
+import { PostBoardResponseDto } from "apis/response/board";
 
 //                      component: 헤더 레이아웃              //
 export default function Header() {
@@ -179,8 +183,48 @@ export default function Header() {
     //        state: 게시물 상태        //
     const { title, content, boardImageFileList, resetBoard } = useBoardStore();
 
+    //          function: post board response 처리 함수          //
+    const postBoardResponse = (
+      responseBody: PostBoardResponseDto | ResponseDto | null
+    ) => {
+      if (!responseBody) return;
+      const { code } = responseBody;
+      if (code === "AF" || code === "NU") navigate(AUTH_PATH());
+      if (code === "VF") alert("게시물 작성에 실패했습니다.");
+      if (code === "DBE") alert("데이터베이스 오류입니다.");
+      if (code !== "SU") return;
+
+      resetBoard();
+      if (!loginUser) return;
+      const { email } = loginUser;
+      navigate(USER_PATH(email));
+    };
+
     // event handler: 업로드 버튼 클릭 이벤트 처리 함수 //
-    const onUploadButtonClickHandler = () => {};
+    const onUploadButtonClickHandler = async () => {
+      const accessToken = cookies.accessToken;
+      if (!accessToken) return;
+
+      const boardImageList: string[] = [];
+
+      for (const file of boardImageFileList) {
+        const data = new FormData();
+        data.append("file", file);
+
+        const url = await fileUploadRequest(data);
+        if (url) {
+          boardImageList.push(url);
+        }
+      }
+
+      const requestBody: PostBoardRequestDto = {
+        title,
+        content,
+        boardImageList,
+      };
+
+      postBoardRequest(requestBody, accessToken).then(postBoardResponse);
+    };
 
     // render: 업로드 버튼 컴포넌트 렌더링    //
     if (title && content) {
