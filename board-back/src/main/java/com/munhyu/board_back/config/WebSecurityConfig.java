d612b358555e2a4db9b166dabd8bcff763292ba4
MYH <ansdudgb@gmail.com>
2025-08-26 18:52:56 +0900

    feat: 스프링 시큐리티에 CORS 설정 통합

--- board-back/src/main/java/com/munhyu/board_back/config/WebSecurityConfig.java
+++ board-back/src/main/java/com/munhyu/board_back/config/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package com.munhyu.board_back.config;
 
 import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -28,7 +27,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
-@Configurable
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
